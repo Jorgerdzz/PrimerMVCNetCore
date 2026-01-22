1e11b61fddd1387377c6459974d3509666ee521f
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-22 10:03:08 +0100

    Enviar informacion Controller --> Vista

---
+++ PrimerMVCNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMVCNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ControladorVista()
+        {
+            //VAMOS A ENVIAR INFORMACION SIMPLE A NUESTRA VISTA
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 20;
+            ViewBag.DiaSemana = "Jueves";
+            return View();
+        }
+
+    }
+}
